7edf70c63de313d0835f81035b41314828093a81
winechit-dev <winechitpaing.dev@gmail.com>
2025-04-21 16:16:07 +0700

    Add task form and routing for task creation

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,12 +1,14 @@
 package com.example
 
 import com.example.model.Priority
+import com.example.model.Task
 import com.example.repository.TaskRepository
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -18,6 +20,7 @@ fun Application.configureRouting() {
     }
     routing {
         staticResources("/content", "mycontent")
+        staticResources("/task-ui", "task-ui")
 
         get("/") {
             call.respondText("Hello Ktor!")
@@ -41,7 +44,7 @@ fun Application.configureRouting() {
             )
         }
 
-        get("/tasks/byPriority/{priority}"){
+        get("/tasks/byPriority/{priority}") {
             val priorityAsText = call.parameters["priority"]
             if (priorityAsText == null) {
                 call.respond(HttpStatusCode.BadRequest)
@@ -61,7 +64,38 @@ fun Application.configureRouting() {
                     contentType = ContentType.parse("text/html"),
                     text = tasks.tasksAsTable()
                 )
-            } catch(ex: IllegalArgumentException) {
+            } catch (ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        }
+
+        post("/tasks") {
+            val formContent = call.receiveParameters()
+
+            val params = Triple(
+                formContent["name"] ?: "",
+                formContent["description"] ?: "",
+                formContent["priority"] ?: ""
+            )
+
+            if (params.toList().any { it.isEmpty() }) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            try {
+                val priority = Priority.valueOf(params.third)
+                TaskRepository.addTask(
+                    Task(
+                        name = params.first,
+                        description = params.second,
+                        priority = priority
+                    )
+                )
+                call.respond(HttpStatusCode.NoContent)
+            } catch (ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (ex: IllegalStateException) {
                 call.respond(HttpStatusCode.BadRequest)
             }
         }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -43,4 +43,34 @@ class ApplicationTest {
         val response = client.get("/tasks/byPriority/Vital")
         assertEquals(HttpStatusCode.NotFound, response.status)
     }
+
+    @Test
+    fun newTasksCanBeAdded() = testApplication {
+        application {
+            module()
+        }
+
+        val response1 = client.post("/tasks") {
+            header(
+                HttpHeaders.ContentType,
+                ContentType.Application.FormUrlEncoded.toString()
+            )
+            setBody(
+                listOf(
+                    "name" to "swimming",
+                    "description" to "Go to the beach",
+                    "priority" to "Low"
+                ).formUrlEncode()
+            )
+        }
+
+        assertEquals(HttpStatusCode.NoContent, response1.status)
+
+        val response2 = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, response2.status)
+        val body = response2.bodyAsText()
+
+        assertContains(body, "swimming")
+        assertContains(body, "Go to the beach")
+    }
 }
